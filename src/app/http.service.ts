5d75f448d68aeb589b03f0226b17d1e282c6beab
Freiraum <freiraum@ungehorsam.ch>
2022-12-19 18:20:06 +0100

    fix lint

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ByOwnerDataAggregation, DataResponse, Squat } from './model/api';
 import { HttpClient } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
