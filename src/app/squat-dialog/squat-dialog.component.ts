caa6b1f7ac75c8387df231e88f40fd3f7d2f07b2
Freiraum <freiraum@ungehorsam.ch>
2022-12-20 15:19:27 +0100

    use google maps for squats

--- src/app/squat-dialog/squat-dialog.component.ts
+++ src/app/squat-dialog/squat-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Squat } from '../model/api';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-squat-dialog',
@@ -8,5 +9,14 @@ import { Squat } from '../model/api';
   styleUrls: ['./squat-dialog.component.scss'],
 })
 export class SquatDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Squat) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Squat,
+    private domSanitizer: DomSanitizer
+  ) {}
+
+  getGoogleMapUrl(query: string): SafeResourceUrl {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(
+      `https://maps.google.com/maps?q=${query}&output=embed&t=h&z=20`
+    );
+  }
 }
