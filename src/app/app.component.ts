5e56fba89c175831cf40ffa45805f487b8c12c52
Freiraum <freiraum@ungehorsam.ch>
2023-01-21 17:16:15 +0100

    updated info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { Spinkit } from 'ng-http-loader';
+import {
+  NgHttpLoaderComponent,
+  Spinkit,
+  SpinnerVisibilityService,
+} from 'ng-http-loader';
 import { MatDialog } from '@angular/material/dialog';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -10,11 +14,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [SpinnerVisibilityService],
 })
-export class AppComponent implements OnInit {
-  title = 'freiraum-map';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('ngHttpLoader')
+  ngHttpLoader!: NgHttpLoaderComponent;
 
   spinkit = Spinkit;
+
+  welcomed = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
@@ -25,10 +33,20 @@ export class AppComponent implements OnInit {
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private spinnerService: SpinnerVisibilityService
   ) {}
 
-  ngOnInit() {
-    this.dialog.open(WelcomeComponent);
+  ngAfterViewInit(): void {
+    this.ngHttpLoader.isVisible$.subscribe(v => {
+      if (!v && !this.welcomed) {
+        this.dialog.open(WelcomeComponent, {
+          width: '100vw',
+          height: '50vh',
+        });
+        this.welcomed = true;
+        console.log('No HTTP requests pending anymore (from ngAfterViewInit)');
+      }
+    });
   }
 }
